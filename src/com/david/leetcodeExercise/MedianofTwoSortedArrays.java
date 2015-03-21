6768bf8c1a023e117cbd5cebdbb7c0e882cd9220
David <choiwaikit.cwj@gmail.com>
2015-03-21 15:46:30 +0800

    substitute the AstartEnd name with some simple names to increase readability

--- src/com/david/leetcodeExercise/MedianofTwoSortedArrays.java
+++ src/com/david/leetcodeExercise/MedianofTwoSortedArrays.java
@@ -45,43 +45,47 @@ public class MedianofTwoSortedArrays {
 	public int getIthElmt(int[] A, int[] B, int rank, int[] AstartEnd, int[] BstartEnd){
 		int indexA;
 		int indexB;
-		int lengthA = AstartEnd[1] - AstartEnd[0]+1;
-		int lengthB = BstartEnd[1] - BstartEnd[0]+1;
-//		System.out.format("rank: %d, Astart:%d, Aend:%d, Bstart:%d, Bend:%d%n",rank,AstartEnd[0], AstartEnd[1], BstartEnd[0],BstartEnd[1]);
+		int Astart = AstartEnd[0];
+		int Aend = AstartEnd[1];
+		int Bstart = BstartEnd[0];
+		int Bend = BstartEnd[1];
+		int lengthA = Aend - Astart+1;
+		int lengthB = Bend - Bstart+1;
+//		System.out.format("rank: %d, Astart:%d, Aend:%d, Bstart:%d, Bend:%d%n",rank,Astart, Aend, Bstart,Bend);
 		if ( rank == 1){
-			return (A[AstartEnd[0]]<=B[BstartEnd[0]]? A[AstartEnd[0]]:B[BstartEnd[0]]);
+			return (A[Astart]<=B[Bstart]? A[Astart]:B[Bstart]);
 		}
 		if (Math.ceil(rank/2.0) > lengthA ){
-			indexA = AstartEnd[1];
-			indexB = ( rank - lengthA -1 ) + BstartEnd[0] ;//since the index is starting from 0
+			indexA = Aend;
+			indexB = ( rank - lengthA -1 ) + Bstart ;//since the index is starting from 0
 		}else if(Math.ceil(rank/2.0) > lengthB) {
-			indexB = BstartEnd[1] ;
-			indexA = ( rank - lengthB - 1 ) + AstartEnd[0];
+			indexB = Bend ;
+			indexA = ( rank - lengthB - 1 ) + Astart;
 		}else{
-			indexA = (int)Math.ceil(rank/2.0) + AstartEnd[0] -1;
-			indexB = (int)Math.floor(rank/2.0) + BstartEnd[0] -1;
+			indexA = (int)Math.ceil(rank/2.0) + Astart -1;
+			indexB = (int)Math.floor(rank/2.0) + Bstart -1;
 		}
 //		System.out.format("Aindex:%d, Bindex: %d%n", indexA, indexB);
 //		System.out.format("A[indexA]:%d, B[indexB]:%d%n", A[indexA],B[indexB]);
 		if ( A[indexA] > B[indexB]){
-			if ( indexB == BstartEnd[1]){
-				int elmtInB = indexB - BstartEnd[0] + 1;
-				return A[rank - elmtInB + AstartEnd[0] - 1];
+			if ( indexB == Bend){
+				int elmtInB = indexB - Bstart + 1;
+				return A[rank - elmtInB + Astart - 1];
 			}
-			int newRank = rank - ( indexB - BstartEnd[0] + 1 );
-			int[] newAstartEnd = { AstartEnd[0] , indexA };
-			int[] newBstartEnd = { indexB + 1,BstartEnd[1]};
-//			System.out.format("newrank: %d, newAstart:%d, newAend:%d, newBstart:%d, newBend:%d%n",newRank,newAstartEnd[0], newAstartEnd[1], newBstartEnd[0],newBstartEnd[1]);
+			int newRank = rank - ( indexB - Bstart + 1 );
+			int[] newAstartEnd = { Astart , indexA };
+			int[] newBstartEnd = { indexB + 1,Bend};
+//			System.out.format("newrank: %d, newAstart:%d, newAend:%d, newBstart:%d, newBend:%d%n",newRank,newAstart, newAend, newBstart,newBend);
 			return getIthElmt(A, B, newRank, newAstartEnd ,newBstartEnd);
 		}else if(A[indexA] < B[indexB]) {
-			if ( indexA == AstartEnd[1]){
-				int elmtInA = indexA - AstartEnd[0] + 1;
-				return B[rank - elmtInA +BstartEnd[0]-1];
+			if ( indexA == Aend){
+				int elmtInA = indexA - Astart + 1;
+				return B[rank - elmtInA +Bstart-1];
 			}
-			int newRank = rank - (indexA - AstartEnd[0] +1 ); //indexA means how many elements are in from of A[indexA]
-			int[] newBstartEnd = { BstartEnd[0] , indexB };
-			int[] newAstartEnd = { indexA + 1,AstartEnd[1]};
-//			System.out.format("newrank: %d, newAstart:%d, newAend:%d, newBstart:%d, newBend:%d%n",newRank,newAstartEnd[0], newAstartEnd[1], newBstartEnd[0],newBstartEnd[1]);
+			int newRank = rank - (indexA - Astart +1 ); //indexA means how many elements are in from of A[indexA]
+			int[] newBstartEnd = { Bstart , indexB };
+			int[] newAstartEnd = { indexA + 1,Aend};
+//			System.out.format("newrank: %d, newAstart:%d, newAend:%d, newBstart:%d, newBend:%d%n",newRank,newAstart, newAend, newBstart,newBend);
 			return getIthElmt(A, B, newRank, newAstartEnd ,newBstartEnd);
 		}else{
 			return A[indexA];
